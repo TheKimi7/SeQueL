d1183f75b08abddb3c768609df5c588de36ff3c7
TheKimi7 <ece20145@hotmail.com>
2026-01-08 23:18:40 +0530

    Edited

--- customize.sh
+++ customize.sh
@@ -1,1 +1,14 @@
-set_perm $MODPATH/system/bin/sqlite3 0 0 0755
+bindir=/system/bin
+
+ui_print "[0/3] Keeping necessary binary file: $MODPATH$xbindir/sqlite3.$ARCH"
+cp $MODPATH$bindir/sqlite3.$ARCH $MODPATH$bindir/sqlite3.keep
+
+ui_print "[1/3] Deleting unnecessary binary files"
+rm -f $MODPATH$bindir/sqlite3.arm $MODPATH$bindir/sqlite3.arm64 $MODPATH$bindir/sqlite3.x64 $MODPATH$bindir/sqlite3.x86
+
+ui_print "[2/3] Restoring necessary binary file to $MODPATH$xbindir/sqlite3"
+mv $MODPATH$bindir/sqlite3.keep  $MODPATH$bindir/sqlite3
+
+ui_print "[3/3] Installed to $bindir"
+
+set_perm_recursive $MODPATH 0 0 0755 0755
